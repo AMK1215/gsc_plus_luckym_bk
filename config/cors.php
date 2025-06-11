3d2c11f7a02427be45261dce202bd74c317865d4
AMK1215 <aungmyokhaing1215@gmail.com>
2025-06-11 07:56:24 +0630

    url

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:5173'],
+    'allowed_origins' => ['https://gsc-plus-luckym-bk-ui.vercel.app'],
 
     'allowed_origins_patterns' => [],
 
